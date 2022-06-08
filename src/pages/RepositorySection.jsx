6cd48c01e60a82f0b3a30a824e04af841d1d5a16
DzakiyH <43630961+DzakiyH@users.noreply.github.com>
2022-06-08 20:33:22 +0700

    KMNR-12 Finished Making Base Repository Section Page (#14)
    * Added Homepage
    
    * added base repository page
    
    * finished file detail base component
    
    * finishing base Repository Section page

--- src/pages/RepositorySection.jsx
+++ src/pages/RepositorySection.jsx
@@ -3,6 +3,8 @@ import NavigationBar from '../components/common/NavigationBar';
 import Sidebar from '../components/common/Sidebar';
 import BaseCard from '../components/generic/card/BaseCard';
 import Footer from '../components/common/Footer';
+import BaseFileDetail from '../components/generic/filedetail/BaseFileDetail';
+import BaseBreadcrumbs from '../components/generic/breadcrumbs/BaseBreadcrumbs';
 
 function Repository() {
   return (
@@ -11,7 +13,14 @@ function Repository() {
       <div className='w-full flex flex-row flex-grow'>
         <Sidebar />
         <div className='w-full px-12 pt-12'>
+          <BaseBreadcrumbs />
           <BaseCard main={true} />
+          <BaseCard firstSection={true} />
+          <BaseCard secondSection={true} />
+          <div className='flex flex-col mt-24'>
+            <BaseFileDetail />
+            <BaseFileDetail />
+          </div>
         </div>
       </div>
       <Footer />
