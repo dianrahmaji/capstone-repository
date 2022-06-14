ba2dc92f6e27a8a0128d89ad5f3e64620787c492
DzakiyH <dzakiy.h@mail.ugm.ac.id>
2022-06-14 20:26:37 +0700

    finished the search results page base

--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -1,8 +1,39 @@
 import React from 'react';
 import NavigationBar from '../components/common/NavigationBar';
 import BaseFileDetail from '../components/generic/filedetail/BaseFileDetail';
+import BaseStackedLists from '../components/generic/stackedlists/BaseStackedLists';
 
 function SearchResults() {
+  const positions = [
+    {
+      id: 1,
+      title: 'Back End Developer',
+      type: 'Full-time',
+      location: 'Remote',
+      department: 'Engineering',
+      closeDate: '2020-01-07',
+      closeDateFull: 'January 7, 2020',
+    },
+    {
+      id: 2,
+      title: 'Front End Developer',
+      type: 'Full-time',
+      location: 'Remote',
+      department: 'Engineering',
+      closeDate: '2020-01-07',
+      closeDateFull: 'January 7, 2020',
+    },
+    {
+      id: 3,
+      title: 'User Interface Designer',
+      type: 'Full-time',
+      location: 'Remote',
+      department: 'Design',
+      closeDate: '2020-01-14',
+      closeDateFull: 'January 14, 2020',
+    },
+  ];
+
   return (
     <div>
       <NavigationBar />
@@ -17,8 +48,9 @@ function SearchResults() {
         </div>
         <div className='text-4xl text-blue-400 font-bold'>Repositories</div>
         <div className='flex flex-col gap-y-8 my-12'>
-          <BaseFileDetail />
-          <BaseFileDetail />
+          {positions.map((position) => (
+            <BaseStackedLists position={position} />
+          ))}
         </div>
       </div>
     </div>
